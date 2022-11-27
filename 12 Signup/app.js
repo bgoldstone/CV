e853c771a614793ec76c0529ad281fa7e9f44d77
Benjamin Goldstone <bgoldstone1@gmail.com>
2022-11-27 17:00:59 -0500

    updated signup

--- 12 Signup/app.js
+++ 12 Signup/app.js
@@ -3,14 +3,20 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const path = require("path");
 const app = express();
+const https = require("https");
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "./public/")));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./signup.html"));
 });
 
+app.post("/", (req, res) => {
+  console.log(JSON.stringify(req.body));
+  res.redirect("/");
+});
 app.listen(port, () => {
   console.log("App listening on port " + port);
 });
