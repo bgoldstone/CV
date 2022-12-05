d75e64618e258cd0e76562673bf9887842b20248
Benjamin Goldstone <bgoldstone1@gmail.com>
2022-12-05 17:30:01 -0500

    added project3

--- 13 EJS/app.js
+++ 13 EJS/app.js
@@ -3,20 +3,21 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = 3000;
 const app = express();
+const items = ["Send Emails", "Buy Milk", "Take the trash out"];
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "./public/")));
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+let today = new Date();
+const options = { weekday: "long", day: "numeric", month: "long" };
 app.get("/", (req, res) => {
-  res.send(`Today is ${days[new Date().getDay()]}`);
+  res.render("index", {
+    day: today.toLocaleDateString("en-US", options),
+    items: items,
+  });
+});
+app.post("/", (req, res) => {
+  items.push(req.body.newItem);
+  res.redirect("/");
 });
 app.listen(PORT, () =>
   console.log(`Server listening on  http://localhost:${PORT}`)
